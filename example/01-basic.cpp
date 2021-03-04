0137e2a09b0b54dff1f2747d683abc4da107e606
Martin Moene <martin.moene@gmail.com>
2021-03-04 16:51:14 +0100

    [skip ci] Add example

---
+++ example/01-basic.cpp
@@ -0,0 +1,23 @@
+#include "nonstd/jthread.hpp"
+
+#include <iostream>
+#include <thread>
+
+int main()
+{
+    std::cout << std::endl << std::boolalpha;
+
+#if defined(THREAD_STD)
+    std::thread thr{[]{ std::cout << "Joinable std::thread" << std::endl; }};
+#elif defined(JTHREAD_STD)
+    std::jthread thr{[]{ std::cout << "Joinable std::jthread" << std::endl; }};
+#else
+    nonstd::jthread thr{[]{ std::cout << "Joinable nonstd::thread" << std::endl; }};
+#endif
+
+    std::cout << "thr.joinable(): " << thr.joinable() << "\n" << std::endl;
+}
+
+// define: THREAD_STD, JTHREAD_STD, none/other
+// clang-cl -DJTHREAD_STD -std:c++latest -MTd -EHsc -W3 -I../include/ 01-basic.cpp && 01-basic.exe
+// g++ -DJTHREAD_STD -std=c++20 -Wall -I../include/ 01-basic.cpp && 01-basic.exe
