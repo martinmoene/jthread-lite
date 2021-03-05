d3e3fad033f192b7e8e1061c11ea145727acdad1
Martin Moene <martin.moene@gmail.com>
2021-03-05 17:23:30 +0100

    Add example 2

---
+++ example/02-arguments.cpp
@@ -0,0 +1,20 @@
+#include "nonstd/jthread.hpp"
+#include <iostream>
+
+int main(int argc, char **)
+{
+    int product = 0;
+    const int six = 6;
+    const int seven = 7;
+
+    {
+        nonstd::jthread thr{[&](int x, int y){ product = x * y; }, six, seven };
+
+        // automatically join thread here, making sure it has executed:
+    }
+
+    std::cout << "Product of "<< six << " and " << seven << " is " << product << ".\n";
+}
+
+// clang-cl -std:c++latest -MTd -EHsc -W3 -I../include/ 02-arguments.cpp && 02-arguments.exe
+// g++ -std=c++2a -Wall -I../include/ -o 02-arguments.exe 02-arguments.cpp && 02-arguments.exe
