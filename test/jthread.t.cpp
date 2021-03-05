dccfd379abc45b09e6acf04ccdb45210deb3c127
Martin Moene <martin.moene@gmail.com>
2021-03-05 09:47:30 +0100

    Add test for construction of thread

--- test/jthread.t.cpp
+++ test/jthread.t.cpp
@@ -1,6 +1,15 @@
 #include "jthread-main.t.hpp"
 
-CASE("jthread: ")
+CASE("jthread: default construct")
 {
-    EXPECT( true );
+    nonstd::jthread thr;
+
+    EXPECT( thr.get_id() != std::this_thread::get_id() );
+}
+
+CASE("jthread: create thread with callback")
+{
+    nonstd::jthread thr{[]{} };
+
+    EXPECT( thr.joinable() );
 }
