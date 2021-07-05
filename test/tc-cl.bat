e21e7530d964db954d4ecd9e42eb310ee41bd084
Martin Moene <martin.moene@gmail.com>
2021-07-05 17:12:34 +0200

    Add script tc-cl.bat (nonstd-lite-project issues 54)

--- test/tc-cl.bat
+++ test/tc-cl.bat
@@ -1,36 +1,40 @@
 @echo off & setlocal enableextensions enabledelayedexpansion
 ::
-:: tcl.bat - compile & run tests (clang-cl).
+:: tc-cl.bat - compile & run tests (clang-cl).
 ::
 
 set      unit=jthread
-set unit_file=jthread
+set unit_file=%unit%
 
 :: if no std is given, use c++14
 
 set std=c++14
 if NOT "%1" == "" set std=%1 & shift
 
-set select_jthread=jthread_CONFIG_SELECT_JTHREAD_NONSTD
-if NOT "%1" == "" set select_jthread=%1 & shift
+set UCAP=%unit%
+call :toupper UCAP
+
+set unit_select=%unit%_%UCAP%_NONSTD
+::set unit_select=%unit%_CONFIG_SELECT_%UCAP%_NONSTD
+if NOT "%1" == "" set unit_select=%1 & shift
 
 set args=%1 %2 %3 %4 %5 %6 %7 %8 %9
 
 set  clang=clang-cl
 
 call :CompilerVersion version
-echo %clang% %version%: %std% %select_jthread% %args%
+echo %clang% %version%: %std% %unit_select% %args%
 
 set unit_config=^
-    -Djthread_JTHREAD_HEADER=\"nonstd/jthread.hpp\" ^
-    -Djthread_TEST_NODISCARD=0 ^
-    -Djthread_CONFIG_SELECT_JTHREAD=%select_jthread%
+    -D%unit%_%UCAP%_HEADER=\"nonstd/%unit%.hpp\" ^
+    -D%unit%_TEST_NODISCARD=0 ^
+    -D%unit%_CONFIG_SELECT_%UCAP%=%unit_select%
 
 rem -flto / -fwhole-program
 set  optflags=-O2
-set warnflags=-Wall -Wextra -Wpedantic -Weverything -Wshadow -Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-padded -Wno-missing-noreturn -Wno-documentation-unknown-command -Wno-documentation-deprecated-sync -Wno-documentation -Wno-weak-vtables -Wno-missing-prototypes -Wno-missing-variable-declarations -Wno-exit-time-destructors -Wno-global-constructors -Wno-sign-conversion -Wno-sign-compare -Wno-implicit-int-conversion -Wno-deprecated-declarations
+set warnflags=-Wall -Wextra -Wpedantic -Weverything -Wshadow -Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-padded -Wno-missing-noreturn -Wno-documentation-unknown-command -Wno-documentation-deprecated-sync -Wno-documentation -Wno-weak-vtables -Wno-missing-prototypes -Wno-missing-variable-declarations -Wno-exit-time-destructors -Wno-global-constructors -Wno-sign-conversion -Wno-sign-compare -Wno-implicit-int-conversion -Wno-deprecated-declarations -Wno-date-time
 
-"%clang%" -m32 -EHsc -std:%std% %optflags% %warnflags% %unit_config% -fms-compatibility-version=19.00 /imsvc lest -I../include -Ics_string -I. -o %unit_file%-main.t.exe %unit_file%-main.t.cpp %unit_file%.t.cpp && %unit_file%-main.t.exe
+"%clang%" -EHsc -std:%std% %optflags% %warnflags% %unit_config% -fms-compatibility-version=19.00 /imsvc lest -I../include -Ics_string -I. -o %unit_file%-main.t.exe %unit_file%-main.t.cpp %unit_file%.t.cpp && %unit_file%-main.t.exe
 endlocal & goto :EOF
 
 :: subroutines:
